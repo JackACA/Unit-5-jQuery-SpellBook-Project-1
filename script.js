c93ddea88894180a20b34ddf0c2f53f83a67934f
JackACA <43751812+JackACA@users.noreply.github.com>
2019-01-14 10:07:31 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".button1").click(function(){
+ $("#water-bottle").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$(".button2").click(function(){
+ $("#card").toggle();
+});
+
+
+
+//Spell #3
+
+$(".button3").click(function(){
+  $("#book").slideToggle();
+});
+
+
+
